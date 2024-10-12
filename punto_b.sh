71c44859a351ae7a66ddb7049690ec4038a606f5
Thiaguito2024 <thiagojlgd@gmail.com>
2024-10-12 03:01:19 +0000

    Archivos resueltos del parcial

--- punto_b.sh
+++ punto_b.sh
@@ -1,1 +1,10 @@
-Completar
+sudo fdisk -l
+sudo fdisk /dev/sda
+sudo mkfs.ext4 /dev/sda1
+sudo mkfs.ext4 /dev/sda2
+sudo mkfs.ext4 /dev/sda3
+sudo mkfs.ext4 /dev/sda4
+sudo mount /dev/sda1 /mnt/part1
+sudo mount /dev/sda2 /mnt/part2
+sudo mount /dev/sda3 /mnt/part3
+sudo mount /dev/sda4 /mnt/part4
